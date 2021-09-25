c7e4a5f0cdfbe3bba7663f4ee0a33b393a1c2d93
mbakulev <michael.baculev@gmail.com>
2021-09-25 17:36:36 +0400

    second commit

--- src/components/bus/index.js
+++ src/components/bus/index.js
@@ -1,37 +1,85 @@
-import { Table } from "react-bootstrap"
+import { Modal, Form, Button } from "react-bootstrap"
+import BootstrapTable from 'react-bootstrap-table-next'
+import { useState } from "react"
 
 const BusTable = () => {
+    const [modal, setModal] = useState(false)
+    const [modalDataIndex, setModalDataIndex] = useState(0)
+
+    const columns = [
+        {
+            dataField: 'id',
+            text: '#'
+        },
+        {
+            dataField: 'busNumber',
+            text: 'Номер автобуса'
+        },
+        {
+            dataField: 'countWithoutTicket',
+            text: 'Количество без билетов',
+        },
+        {
+            dataField: 'percentWithoutTicket',
+            text: '% без билетов'
+        },
+        {
+            dataField: 'brigade',
+            text: 'Номер бригады'
+        },
+        {
+            dataField: 'checksCount',
+            text: 'Количество проверок'
+        }
+    ];
+    const data = [
+        {
+            id: '1',
+            busNumber: '1',
+            countWithoutTicket: '100',
+            percentWithoutTicket: '10',
+            brigade: '2',
+            checksCount: '123'
+        },
+        {
+            id: '2',
+            busNumber: '2',
+            countWithoutTicket: '100',
+            percentWithoutTicket: '20',
+            brigade: '1',
+            checksCount: '165'
+        },
+    ]
+    const rowEvents = {
+        onClick: (e, row, rowIndex) => {
+            setModal(true)
+            setModalDataIndex(rowIndex)
+        }
+    }
     return (
         <div style={{ "margin": "50px", "padding": "50px" }}>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Username</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td colSpan="2">Larry the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
-                </tbody>
-            </Table>
+            <BootstrapTable keyField='id' data={data} columns={columns} rowEvents={rowEvents} />
+            <Modal
+                show={modal}
+                onHide={() => setModal(false)}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="example-modal-sizes-title-lg">
+                        Добавть ролик
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group controlId="formFile" className="mb-3">
+                            <Form.Label>Добавить ролик для монитора автобуса</Form.Label>
+                            <Form.Control type="file" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Добавить
+                        </Button>
+                    </Form>
+                </Modal.Body>
+            </Modal>
         </div>
     )
 }
--- src/components/tasks/index.js
+++ src/components/tasks/index.js
@@ -1,38 +1,11 @@
 import { useState } from "react"
-import { Table, Button, Form } from "react-bootstrap"
+import { Button } from "react-bootstrap"
 import { Modal } from "react-bootstrap"
 import BootstrapTable from 'react-bootstrap-table-next'
+import { Form, Field } from "react-final-form"
 
 const TasksTable = () => {
     const [isModal, setModal] = useState(false)
-    // const [brigadeNumber, setBrigadeNumber] = useState(),
-    //     onInputBrigade = ({ target: { value } }) => setBrigadeNumber(value),
-    //     onFormSubmit = e => {
-    //         e.preventDefault()
-    //         console.log(brigadeNumber)
-    //         setBrigadeNumber()
-    //     }
-    // const [districtNumber, setDistrictNumber] = useState(),
-    //     onInputDistrict = ({ target: { value } }) => setDistrictNumber(value),
-    //     onFormSubmit = e => {
-    //         e.preventDefault()
-    //         console.log(value)
-    //         setDistrictNumber()
-    //     }
-    // const [value, setValue] = useState(),
-    //     onInput = ({ target: { value } }) => setValue(value),
-    //     onFormSubmit = e => {
-    //         e.preventDefault()
-    //         console.log(value)
-    //         setValue()
-    //     }
-    // const [value, setValue] = useState(),
-    //     onInput = ({ target: { value } }) => setValue(value),
-    //     onFormSubmit = e => {
-    //         e.preventDefault()
-    //         console.log(value)
-    //         setValue()
-    //     }
     const columns = [
         {
             dataField: 'id',
@@ -55,64 +28,39 @@ const TasksTable = () => {
             text: 'Остановка автобуса'
         }
     ];
-    const data = [
-        {
-            id: '1',
-            brigade: 'Бригада 1',
-            district: '100',
-            busNumber: '1',
-            busStop: 'Вишневая'
-        },
-        {
-            id: '2',
-            brigade: 'Бригада 2',
-            district: '200',
-            busNumber: '2',
-            busStop: 'Малиновая'
-        },
-    ]
-    const handleSubmitForm = (evt) => {
-        console.log(evt)
-    }
-
+    const [tableData, setTableData] = useState([{
+        id: '1',
+        brigade: 'Бригада 1',
+        district: '100',
+        busNumber: '1',
+        busStop: 'Вишневая'
+    },
+    {
+        id: '2',
+        brigade: 'Бригада 2',
+        district: '200',
+        busNumber: '2',
+        busStop: 'Малиновая'
+    }])
+    const onSubmit = (values) => {
+        let result = tableData
+        result.push({
+            id: tableData.length + 1,
+            brigade: values?.brigade,
+            district: values?.district,
+            busNumber: values?.busNumber,
+            busStop: values?.busStop,
+        },)
+        setTableData(result)
+        setModal(false)
+      };
     return (
         <div style={{ "margin": "50px", "padding": "50px" }}>
             <Button variant="primary" style={{ "marginBottom": "50px" }} onClick={() => setModal(true)}>Добавить задачу</Button>
-            <BootstrapTable keyField='id' data={data} columns={columns} />
-            {/* <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Username</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td colSpan="2">Larry the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
-                </tbody>
-            </Table> */}
+            <BootstrapTable keyField='id' data={tableData} columns={columns} />
             <Modal
-                size="lg"
                 show={isModal}
                 onHide={() => setModal(false)}
-                aria-labelledby="example-modal-sizes-title-lg"
             >
                 <Modal.Header closeButton>
                     <Modal.Title id="example-modal-sizes-title-lg">
@@ -120,51 +68,53 @@ const TasksTable = () => {
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <Form onClick={handleSubmitForm}>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                            <Form.Text className="text-muted">
-                                We'll never share your email with anyone else.
-                            </Form.Text>
-                        </Form.Group>
-
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Check me out" />
-                        </Form.Group>
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
-                    </Form>
-                    {/* <Form onSubmit={onFormSubmit}>
-                        <Form.Control
-                            type="text"
-                            onChange={onInputBrigade}
-                            value={brigadeNumber}
-                        />
-                        <Form.Control
-                            type="text"
-                            onChange={onInputDistrict}
-                            value={districtNumber}
-                        />
-                        <Form.Control
-                            type="text"
-                            onChange={onInput}
-                            value={value}
-                        />
-                        <Form.Control
-                            type="text"
-                            onChange={onInput}
-                            value={value}
-                        />
-                        <Button type="submit">
-                            Submit
-                        </Button>
-                    </Form> */}
+                    <Form
+                        onSubmit={onSubmit}
+                        render={({ handleSubmit }) => (
+                            <form 
+                                onSubmit={handleSubmit}
+                                className="myForm"
+                            >
+                                <Field name="brigade">
+                                    {({ input }) => (
+                                        <div>
+                                            <div style={{"padding": "5px"}}><label>Номер бригады</label></div>                      
+                                            <div style={{"padding": "5px"}}><input {...input} type="text" placeholder="Номер бригады"/></div>
+                                        </div>
+                                    )}
+                                </Field>
+                                <Field name="district">
+                                    {({ input }) => (
+                                        <div>
+                                            <div style={{"padding": "5px"}}><label>Район</label></div>
+                                            <div style={{"padding": "5px"}}><input {...input} type="text" placeholder="Район"/></div>
+                                        </div>
+                                    )}
+                                </Field>
+                                <Field name="busNumber">
+                                    {({ input }) => (
+                                        <div>
+                                            <div style={{"padding": "5px"}}><label>Номер автобуса</label></div>
+                                            <div style={{"padding": "5px"}}><input {...input} type="text" placeholder="Номер автобуса"/></div>
+                                        </div>
+                                    )}
+                                </Field>
+                                <Field name="busStop">
+                                    {({ input }) => (
+                                        <div>
+                                            <div style={{"padding": "5px"}}><label>Остановка</label></div>
+                                            <div style={{"padding": "5px"}}><input {...input} type="text" placeholder="Остановка"/></div>
+                                        </div>
+                                    )}
+                                </Field>
+                                <div  style={{"padding": "5px"}}>
+                                    <Button type="submit">
+                                        Создать
+                                    </Button>
+                                </div>
+                            </form>
+                        )}
+                    />
                 </Modal.Body>
             </Modal>
         </div>
